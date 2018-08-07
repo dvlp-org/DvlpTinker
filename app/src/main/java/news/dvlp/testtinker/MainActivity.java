70f5462b85635de6c23d64f8ea4847f5621b3554
liubaigang <mrdlp@sina.com>
2018-08-07 15:54:41 +0800

    添加微信登录

--- app/src/main/java/news/dvlp/testtinker/MainActivity.java
+++ app/src/main/java/news/dvlp/testtinker/MainActivity.java
@@ -7,6 +7,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.tencent.mm.opensdk.modelmsg.SendAuth;
+import com.tencent.mm.opensdk.openapi.IWXAPI;
+import com.tencent.mm.opensdk.openapi.WXAPIFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,12 +23,20 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "我确认点击了", Toast.LENGTH_SHORT).show();
                 // TODO Auto-generated method stub
                 changeIcon();
+//                setRoundIcon();
                 Intent intent = new Intent(Intent.ACTION_MAIN);//模拟Home键点击
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 intent.addCategory(Intent.CATEGORY_HOME);
                 startActivity(intent);
+            }
+        });
+        ((Button)findViewById(R.id.btn2)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WXLogin();
             }
         });
     }
@@ -34,12 +47,44 @@ public class MainActivity extends AppCompatActivity {
 //        pm.setComponentEnabledSetting(getComponentName(),
 //                PackageManager.COMPONENT_ENABLED_STATE_DEFAULT,
 //                PackageManager.DONT_KILL_APP);
+        pm.setComponentEnabledSetting(getComponentName(),
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                PackageManager.DONT_KILL_APP);
         //显示新图标
         pm.setComponentEnabledSetting(new ComponentName(
                         getBaseContext(),
-                        "news.dvlp.testtinker.MainActivity2"),
+                        "news.dvlp.testtinker.MainActivity3"),
                 PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
                 PackageManager.DONT_KILL_APP);
     }
 
+    private void changeIcon2() {
+        PackageManager pm = getApplicationContext().getPackageManager();
+        //去除旧图标，否则会出现2个图标
+        pm.setComponentEnabledSetting(getComponentName(),
+                PackageManager.COMPONENT_ENABLED_STATE_DEFAULT,
+                PackageManager.DONT_KILL_APP);
+        //显示新图标
+        pm.setComponentEnabledSetting(new ComponentName(
+                        getBaseContext(),
+                        "news.dvlp.testtinker.MainActivity3"),
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                PackageManager.DONT_KILL_APP);
+    }
+
+    /**
+     * 登录微信
+     */
+    // 微信登录
+    private static IWXAPI WXapi;
+    private String WX_APP_ID = "wx6397da1a5719b713";
+    private void WXLogin() {
+        WXapi = WXAPIFactory.createWXAPI(this, WX_APP_ID, true);
+        WXapi.registerApp(WX_APP_ID);
+        SendAuth.Req req = new SendAuth.Req();
+        req.scope = "snsapi_userinfo";
+        req.state = "wechat_sdk_demo";
+        WXapi.sendReq(req);
+
+    }
 }
